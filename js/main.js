556ea99234c37f8a05c3c6d74301f33e83d26837
Andrew Gonzalez <andrew.gonzalez@gmail.com>
2016-10-19 19:49:03 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// console.log("Connected to Javascript!")
+// $(window).scroll(function(){
+//     var fromTopPx = 500; // distance to trigger
+//     var scrolledFromtop = $(window).scrollTop();
+//     if(scrolledFromtop > fromTopPx){
+//         $('body').addClass('scrolled');
+//     }else{
+//         $('body').removeClass('scrolled');
+//     }
+// });
